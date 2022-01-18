ecc6c664299126b0998edf74ed06401745f75495
Yaser Ibrahim <yaser@Yasers-MacBook-Pro.local>
2022-01-18 16:00:58 -0500

    add initial content for sidebar in application.js, add styling on button component, conditional css for button (confirm and danger)

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,5 +3,16 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   let buttonClass = "button";
+
+   if (props.confirm) {
+     buttonClass += " button--confirm";
+   } else if (props.danger) {
+      buttonClass += " button--danger";
+   }
+   return (
+   <button className={buttonClass}>
+      {props.children}
+   </button>
+   );
 }
