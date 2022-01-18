7ef0a37c7ed2d117bed91a1363c88b34b70257c4
Yaser Ibrahim <yaser@Yasers-MacBook-Pro.local>
2022-01-18 18:30:01 -0500

    create new components for daylistitem and daylist, update stories with new daylist info

---
+++ src/components/DayListItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function DayListItem(props) {
+  return (
+    <li onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{props.spots} spots remaining</h3>
+    </li>
+  );
+}
+// const dayListItem = [
+//   {
+//     name: "Yaser",
+//     spots: 1,
+//     selected: Boolean,
+//     setDay: function(){
+
+//     }
+//   }
+// ]
