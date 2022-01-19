31e5e910e57cf973ce11249eee72e63717f1a6ac
Yaser Ibrahim <yaser@Yasers-MacBook-Pro.local>
2022-01-18 20:37:33 -0500

    styling the DayListItem

---
+++ src/components/DayList.js
@@ -0,0 +1,10 @@
+// import React from "react";
+// import classNames from "classnames";
+// import dayListItem from "./DayListItem";
+
+// export default function DayList(props) {
+//   const { id, name, spots } = props
+//   return (
+    
+//   );
+// }
