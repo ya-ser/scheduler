a431225217458e956c8935228ed0f8cf8912e71e
Yaser Ibrahim <yaser@Yasers-MacBook-Pro.local>
2022-01-19 11:52:48 -0500

    build out DayList component to pass each day

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,10 +1,23 @@
-// import React from "react";
-// import classNames from "classnames";
-// import dayListItem from "./DayListItem";
+import React from "react";
+import classNames from "classnames";
+import DayListItem from "./DayListItem";
 
-// export default function DayList(props) {
-//   const { id, name, spots } = props
-//   return (
-    
-//   );
-// }
+
+export default function DayList(props) {
+  const { days, day, setDay } = props;
+  const parsedDays = days.map((oneDay) => {
+    return <DayListItem 
+      id={oneDay.id}
+      name={oneDay.name}
+      spots={oneDay.spots}
+      selected={oneDay.name === day}
+      setDay={setDay}
+    />
+  })
+
+  return (
+    <ul>
+      {parsedDays}
+    </ul>
+  );
+}
--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,11 +6,21 @@ export default function DayListItem(props) {
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
-  })
+  });
+  
+  const formatSpots = function() {
+    if (props.spots === 0) {
+      return "no spots remaining";
+    } else if (props.spots === 1) {
+      return "1 spot remaining";
+    } else {
+      return `${props.spots} spots remaining`;
+    }
+  };
   return (
-    <li className={dayClass}onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots()}</h3>
     </li>
   );
 }
